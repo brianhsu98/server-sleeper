7c469dc3c176fa7c8a08fc09442c0672e31351b5
Brian Hsu <brian@brianhsu.me>
2023-12-02 17:27:00 -0800

    Implement initial waker

--- waker/main.go
+++ waker/main.go
@@ -2,29 +2,80 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"os/exec"
 	"time"
+
+	"github.com/go-ping/ping"
 )
 
-func pingIP(ip string) bool {
-	timeout := time.Duration(1 * time.Second)
-	_, err := net.DialTimeout("tcp", ip+":80", timeout)
+func pingIP(ip string) (bool, error) {
+	pinger, err := ping.NewPinger(ip)
 	if err != nil {
-		return false
+		return false, err
 	}
-	return true
+	pinger.Count = 1
+	pinger.Timeout = time.Second
+	err = pinger.Run() // Blocks until finished.
+
+	if err != nil {
+		return false, err
+	}
+
+	stats := pinger.Statistics() // get send/receive/duplicate/rtt stats
+
+	return stats.PacketsRecv > 0, nil
 }
 
-func main() {
-	ip := "10.0.0.72"
+func wakeOnLan(macAddr string) error {
+	cmd := exec.Command("wakeonlan", macAddr)
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
 
+	return nil
+}
+
+func waker(macAddr string, wakeCh <-chan struct{}) {
+	for {
+		select {
+		case <-wakeCh:
+			go func() {
+				err := wakeOnLan(macAddr)
+				if err != nil {
+					fmt.Errorf("%s", err)
+				}
+			}()
+		}
+	}
+}
+
+func ipWatcher(ipAddr string, wakeCh chan struct{}) {
 	interval := 1 * time.Second // Set the interval for the ticker
 	ticker := time.NewTicker(interval)
 	for {
 		select {
 		case <-ticker.C:
-			res := pingIP(ip)
-			fmt.Printf("Ping to %s result: %v\n", ip, res)
+			res, err := pingIP(ipAddr)
+			if err != nil {
+				fmt.Errorf("Error while pinging: %s", err)
+			}
+
+			if res {
+				wakeCh <- struct{}{}
+			} else {
+				fmt.Printf("IP address %s is not active", ipAddr)
+			}
 		}
 	}
+
+}
+
+func main() {
+	ipAddr := "10.0.0.72"
+	macAddr := "6C:4B:90:4B:7B:91"
+	wakeCh := make(chan struct{})
+
+	go ipWatcher(ipAddr, wakeCh)
+	go waker(macAddr, wakeCh)
 }
